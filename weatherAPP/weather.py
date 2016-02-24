5dfc04c618ab9f5e7fc9b0f174308bbefc03c147
CrazeeIvan <ciaranmaher@gmail.com>
2016-02-24 23:27:09 +0000

    v0.3 - Refactored names for clarity and added an exit button.

--- weatherAPP/weather.py
+++ weatherAPP/weather.py
@@ -6,13 +6,16 @@ def getWeather():
     weather_com_result = pywapi.get_weather_from_weather_com('EIXX0014:1:EI')
     report = "It is " + weather_com_result['current_conditions']['text'].lower() + " and " + weather_com_result['current_conditions']['temperature'] + "°C now in Dublin."
     return report
-
+#city = input("Enter city name: ")
 dublinReport = getWeather()
 root = tk.Tk()
 root.title("Ciaran's simple weather app")
 root.geometry("400x300")
-label = tk.Label(root, text=dublinReport, fg="dark green")
-label.pack()
-button = tk.Button(root, text='Update', width=25, command=getWeather())
-button.pack()
+lblReport = tk.Label(root, text=dublinReport, fg="dark green")
+lblReport.pack()
+btnUpdate = tk.Button(root, text='Update', width=25, command=getWeather())
+btnUpdate.pack()
+btnExit = tk.Button(root, text='Exit', width=25, command=root.destroy)
+btnExit.pack()
+
 root.mainloop()
