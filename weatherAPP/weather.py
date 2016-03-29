ba9defd812cb670f9d4d7b8c775368c9df9cfdcc
CrazeeIvan <ciaranmaher@gmail.com>
2016-03-30 00:03:37 +0100

    v0.4 - Added Button for Tullamore as well as Dublin. Actually fixed a bug where look-up method was being called in the declaration. *To do* + Add search look-up using textbox + Add tests + Add simple map GUI

--- weatherAPP/weather.py
+++ weatherAPP/weather.py
@@ -1,20 +1,40 @@
+###############################################################################
+#Tutorial from
+#http://thecodeinn.blogspot.ie/2013/07/tutorial-weather-forecast-in-python.html
+###############################################################################
 import pywapi
 import tkinter as tk
-def getWeather():
-#    if (city == Dublin):
+
+report = "Please select where you'd like your weather report for!"
+
+def getWeatherTullamore():
+    weather_com_result = pywapi.get_weather_from_weather_com('EIXX0046:1:EI')
+    r = "It is " + weather_com_result['current_conditions']['text'].lower() + " and " + weather_com_result['current_conditions']['temperature'] + "°C now in Tullamore."
+    lblReport.config(text=r)
+
+
+def getWeatherDublin():
     weather_com_result = pywapi.get_weather_from_weather_com('EIXX0014:1:EI')
-    weather_com_result = pywapi.get_weather_from_weather_com('EIXX0014:1:EI')
-    report = "It is " + weather_com_result['current_conditions']['text'].lower() + " and " + weather_com_result['current_conditions']['temperature'] + "°C now in Dublin."
-    return report
-#city = input("Enter city name: ")
-dublinReport = getWeather()
+    r = "It is " + weather_com_result['current_conditions']['text'].lower() + " and " + weather_com_result['current_conditions']['temperature'] + "°C now in Dublin."
+    lblReport.config(text=r)
+
+
+
 root = tk.Tk()
 root.title("Ciaran's simple weather app")
 root.geometry("400x300")
-lblReport = tk.Label(root, text=dublinReport, fg="dark green")
+
+lblReport = tk.Label(root, text=report, fg="dark green")
 lblReport.pack()
-btnUpdate = tk.Button(root, text='Update', width=25, command=getWeather())
-btnUpdate.pack()
+
+
+
+btnSelectDublin = tk.Button(root, text='Dublin', width=25, command=getWeatherDublin)
+btnSelectDublin.pack()
+
+btnSelectTullamore = tk.Button(root, text='Tullamore', width=25, command=getWeatherTullamore)
+btnSelectTullamore.pack()
+
 btnExit = tk.Button(root, text='Exit', width=25, command=root.destroy)
 btnExit.pack()
 
